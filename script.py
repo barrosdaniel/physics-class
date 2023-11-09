ab112b3a91c525979903e4dcdd9ec3fbbb69f889
Daniel Barros <daniel.barros@cqumail.com>
2023-11-09 21:03:46 +0800

    feature: CompleteTurn up the Temperature.

--- script.py
+++ script.py
@@ -1,1 +1,22 @@
-print("Started.")
+# Uncomment this when you reach the "Use the Force" section
+# train_mass = 22680
+# train_acceleration = 10
+# train_distance = 100
+# bomb_mass = 1
+
+
+def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * 5 / 9
+    return c_temp
+
+
+def c_to_f(c_temp):
+    f_temp = c_temp * (9 / 5) + 32
+    return f_temp
+
+
+f100_in_celsius = f_to_c(100)
+print(round(f100_in_celsius, 1))
+
+c0_in_fahrenheit = c_to_f(0)
+print(round(c0_in_fahrenheit, 1))
